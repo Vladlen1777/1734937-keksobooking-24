addd95a2075bf1fe1d450975b82b1e1d2545473e
Владлен <vlad.nikolskiy.95@mail.ru>
2021-09-29 16:35:59 +0300

    поработал с линтером

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const getRandomWhole = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
-}
+};
 
 getRandomWhole(20, 35);
 
@@ -15,8 +15,6 @@ getRandomWhole(20, 35);
 https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 */
-const getRandomFraction = (min, max, Digits = 1) => {
-  return Math.random() * (max - min) + min;
-}
+const getRandomFraction = (min, max) => Math.random() * (max - min) + min;
 
 getRandomFraction(1.4, 3.2);
